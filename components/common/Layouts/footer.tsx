b23576a55c13f7da995adf6408bd1469797480f6
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2023-04-03 18:02:50 -0400

    fixed last details in mobile for footer

--- components/common/Layouts/footer.tsx
+++ components/common/Layouts/footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
 	return (
 		<footer className="bg-overlay text-white py-6 w-full relative">
 			<div className="flex md:flex-row flex-col items-center justify-between gap-4 sm:px-16 px-8 w-full">
-				<h2 className="text-center text-xl titleLogo">
+				<h2 className="text-center sm:text-xl text-md titleLogo">
 					{'< '}Connect with <span className="text-primary"> me</span>
 					{' />'}
 				</h2>
@@ -14,7 +14,7 @@ const Footer: React.FC = () => {
 						href="https://twitter.com/andcont207"
 						target="_blank"
 						rel="noopener noreferrer"
-						className="text-2xl hover:text-primary transition-all duration-500"
+						className="sm:text-2xl text-xl hover:text-primary transition-all duration-500"
 					>
 						<i className="fab fa-twitter"></i>
 					</a>
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
 						href="https://www.linkedin.com/in/andr%C3%A9s-contreras-b9612620b/"
 						target="_blank"
 						rel="noopener noreferrer"
-						className="text-2xl hover:text-primary transition-all duration-500"
+						className="sm:text-2xl text-xl hover:text-primary transition-all duration-500"
 					>
 						<i className="fab fa-linkedin-in"></i>
 					</a>
@@ -31,7 +31,7 @@ const Footer: React.FC = () => {
 						href="https://github.com/AN-Drew207"
 						target="_blank"
 						rel="noopener noreferrer"
-						className="text-2xl hover:text-primary transition-all duration-500"
+						className="sm:text-2xl text-xl hover:text-primary transition-all duration-500"
 					>
 						<i className="fab fa-github"></i>
 					</a>
